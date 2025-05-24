e3870d6656724848deae607a5a37d77d363b24da
mariasunxstyle <tohmian@mail.ru>
2025-05-24 15:13:25 +0300

    Add files via upload

---
+++ steps.py
@@ -0,0 +1,88 @@
+# coding: utf-8
+
+steps = [
+    {"step": 1, "positions": [
+        {"name": "Лицом вверх", "duration_min": 1.5},
+        {"name": "На животе", "duration_min": 1.5},
+        {"name": "Левый бок", "duration_min": 1.0},
+        {"name": "Правый бок", "duration_min": 1.0},
+        {"name": "В тени", "duration_min": 3.0},
+    ]},
+    {"step": 2, "positions": [
+        {"name": "Лицом вверх", "duration_min": 2.0},
+        {"name": "На животе", "duration_min": 2.0},
+        {"name": "Левый бок", "duration_min": 1.0},
+        {"name": "Правый бок", "duration_min": 1.0},
+        {"name": "В тени", "duration_min": 3.0},
+    ]},
+    {"step": 3, "positions": [
+        {"name": "Лицом вверх", "duration_min": 3.0},
+        {"name": "На животе", "duration_min": 3.0},
+        {"name": "Левый бок", "duration_min": 1.5},
+        {"name": "Правый бок", "duration_min": 1.5},
+        {"name": "В тени", "duration_min": 5.0},
+    ]},
+    {"step": 4, "positions": [
+        {"name": "Лицом вверх", "duration_min": 5.0},
+        {"name": "На животе", "duration_min": 5.0},
+        {"name": "Левый бок", "duration_min": 2.5},
+        {"name": "Правый бок", "duration_min": 2.5},
+        {"name": "В тени", "duration_min": 5.0},
+    ]},
+    {"step": 5, "positions": [
+        {"name": "Лицом вверх", "duration_min": 7.0},
+        {"name": "На животе", "duration_min": 7.0},
+        {"name": "Левый бок", "duration_min": 3.0},
+        {"name": "Правый бок", "duration_min": 3.0},
+        {"name": "В тени", "duration_min": 7.0},
+    ]},
+    {"step": 6, "positions": [
+        {"name": "Лицом вверх", "duration_min": 9.0},
+        {"name": "На животе", "duration_min": 9.0},
+        {"name": "Левый бок", "duration_min": 5.0},
+        {"name": "Правый бок", "duration_min": 5.0},
+        {"name": "В тени", "duration_min": 10.0},
+    ]},
+    {"step": 7, "positions": [
+        {"name": "Лицом вверх", "duration_min": 12.0},
+        {"name": "На животе", "duration_min": 12.0},
+        {"name": "Левый бок", "duration_min": 7.0},
+        {"name": "Правый бок", "duration_min": 7.0},
+        {"name": "В тени", "duration_min": 10.0},
+    ]},
+    {"step": 8, "positions": [
+        {"name": "Лицом вверх", "duration_min": 15.0},
+        {"name": "На животе", "duration_min": 15.0},
+        {"name": "Левый бок", "duration_min": 10.0},
+        {"name": "Правый бок", "duration_min": 10.0},
+        {"name": "В тени", "duration_min": 10.0},
+    ]},
+    {"step": 9, "positions": [
+        {"name": "Лицом вверх", "duration_min": 20.0},
+        {"name": "На животе", "duration_min": 20.0},
+        {"name": "Левый бок", "duration_min": 15.0},
+        {"name": "Правый бок", "duration_min": 15.0},
+        {"name": "В тени", "duration_min": 15.0},
+    ]},
+    {"step": 10, "positions": [
+        {"name": "Лицом вверх", "duration_min": 25.0},
+        {"name": "На животе", "duration_min": 25.0},
+        {"name": "Левый бок", "duration_min": 20.0},
+        {"name": "Правый бок", "duration_min": 20.0},
+        {"name": "В тени", "duration_min": 20.0},
+    ]},
+    {"step": 11, "positions": [
+        {"name": "Лицом вверх", "duration_min": 35.0},
+        {"name": "На животе", "duration_min": 35.0},
+        {"name": "Левый бок", "duration_min": 25.0},
+        {"name": "Правый бок", "duration_min": 25.0},
+        {"name": "В тени", "duration_min": 30.0},
+    ]},
+    {"step": 12, "positions": [
+        {"name": "Лицом вверх", "duration_min": 45.0},
+        {"name": "На животе", "duration_min": 45.0},
+        {"name": "Левый бок", "duration_min": 30.0},
+        {"name": "Правый бок", "duration_min": 30.0},
+        {"name": "В тени", "duration_min": 40.0},
+    ]}
+]
