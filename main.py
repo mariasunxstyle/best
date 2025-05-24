0b81cb352e67bb98376ed697bf7ac711982c325c
mariasunxstyle <tohmian@mail.ru>
2025-05-24 15:06:36 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-<PASTE COMPLETE CODE FROM canvas Sunxstyle Final Bot Full HERE>
+# coding: utf-8
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+import asyncio
+import os
+
+API_TOKEN = os.getenv("TOKEN")
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME", "@sunxstyle")
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+# ... Весь код бота из документа "Sunxstyle Full Bot Final"
+# Здесь будет скопирована вся логика из canvas
+
+# Пример заглушки для проверки запуска
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.answer("Привет, солнце! ☀️\nТы в таймере по методу суперкомпенсации.")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
